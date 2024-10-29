882a7b9ee8f1bc6a1bcce0e3a0fc5bb80a49a89a
daniel <dnunezv0@gmail.com>
2024-10-29 09:39:21 -0600

    Update i2c driver

--- src/drivers/I2C.rs
+++ src/drivers/I2C.rs
@@ -1,13 +1,36 @@
 
 use stm32g4::stm32g431::I2C1;
-
+use crate::drivers::gpio;
+use crate::drivers::gpio::GPIOPORT;
 
 pub struct I2C {
 
 }
 
+const TYPE_I2C1_GPIO: gpio::GpioConfig = gpio::GpioConfig {
+    moder: gpio::MODER::AlternateFunction,
+    otyper: gpio::OTYPER::OpenDrain,
+    ospeedr: gpio::OSPEEDR::VeryHigh,
+    pupdr: gpio::PUPDR::PullUp,
+    alf_func_sel: Some(4)
+};
+
+const PA13_I2C1_SDA: gpio::Gpio = gpio::Gpio {
+    port: GPIOPORT::GPIOA,
+    pin_number: 13,
+};
+
+const PA14_I2C1_SDA: gpio::Gpio = gpio::Gpio {
+    port: GPIOPORT::GPIOA,
+    pin_number: 14,
+};
+
 impl I2C {
     pub fn begin(){
+
+    }
+
+    pub fn transmit(){
 
     }
 
@@ -18,7 +41,11 @@ impl I2C {
     pub fn configure_gpio(){
         // PA13 I2C1_SCL AF4
         // PA14 I2C1_SDA AF4
+        PA13_I2C1_SDA.configure(TYPE_I2C1_GPIO);
+        PA14_I2C1_SDA.configure(TYPE_I2C1_GPIO)
     }
+
+    /* Private methods */
 
     fn set_timing_prescaler(){
         // Set PRESC[3:0] bits in I2C_TIMINGR register.
@@ -50,6 +77,22 @@ impl I2C {
         }
     }
 
+    fn start_condition(){
+        unsafe {
+            let port = &*I2C1::ptr();
+            // Generate start condition
+            port.cr2.as_ptr().write(port.cr2.as_ptr().read() | (1 << 13));
+            
+        }
+    }
+
+    fn call_address(adress: u8){
+
+    }
+
+    fn stop_condition(){
+
+    }
 
 
 }
